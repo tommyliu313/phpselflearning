251aabeb2e184716a92934f8ff5b1a6c507b77b8
tommyliu313 <liuchaktin@gmail.com>
2022-09-16 01:44:57 +0800

    20220916 0144

---
+++ ch1/sayhello/hello.php
@@ -0,0 +1,5 @@
+<form action="sayhello.php" method="POST">
+    Your Name: <input type="text" name="user" />
+    <br>
+    <button type="submit">Say Hello</button>
+</form>
---
+++ ch1/sayhello/sayhello.php
@@ -0,0 +1,5 @@
+<?php
+print "Hello,";
+print $_POST['user'];
+print "!";
+?>
